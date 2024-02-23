62b5030b7cc1faea2d7d8cfeef1d0d5935861267
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-02-23 09:23:41 +0500

    Main File , Chap 1 , Chap 2 added

---
+++ Chapter 1/script.js
@@ -0,0 +1,15 @@
+// Chapter 1
+
+// Q1
+alert("Greetings!!")
+
+// Q2
+alert("Error! Pls Enter a valid password.")
+
+// Q3
+alert("Enter to JS Land.. \nHappy Coding")
+
+// Q4 
+alert("Enter to JS Land..")
+alert("Happy Coding")
+
---
+++ Chapter 2/script.js
@@ -0,0 +1,37 @@
+// Chapter 2
+
+// Q1
+var username = "Hamza"
+alert(username)
+
+// Q2
+var myname = "Syed Hamza Qadri"
+alert(myname)
+
+// Q3
+var message = "Hello World"
+alert(message)
+
+// Q4
+var m_name = "Syed Hamza Qadri"
+var age = 18
+var value = "Certified Web Developer"
+alert(m_name)
+alert(age)
+alert(value)
+
+// Q5
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+
+// Q6
+var m_email = "My email address is "
+var email = "qadrihamza871@gmail.com"
+alert(m_email + email)
+
+// Q7
+
+var book = "A smarter way to learn Javascript"
+alert("I am trying to learn from the book " + book)
+
+
