98bbcb6888aaebfb067ad7330ed16e49cf1f2dcc
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-02-27 08:37:28 +0500

    Chap 4 added

---
+++ Chapter 4/script.js
@@ -0,0 +1,21 @@
+// Chapter 2
+
+// Q1
+var age = 18
+alert("I am " + age + " years old")
+
+// Q2
+var visit = 14
+alert("You have visited this website " + visit + " times.")
+
+// Q3
+var birthYear = 2005
+document.write("My birth year is " + birthYear + ".<br>")
+document.write("Data type of my declared variable is Number. <br>")
+
+// Q4
+var visitorName = "Hamza "
+var product = " Shirt"
+var quantity = 5
+document.write(visitorName + " ordered " + quantity + product + "(s) on XYZ Clothing Store.")
+
