b535ad53f7254030bd1a78d477366a6358c61085
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-02 21:48:48 +0500

    Update script.js

--- Chapter 5/script.js
+++ Chapter 5/script.js
@@ -4,11 +4,14 @@
 var num1 = 3
 var num2 = 5
 
+document.write("<h1>Question 1</h1>")
+
 // Addition
 var num3 = num1 + num2
 document.write("Sum of " + num1 + " and " + num2 + " is " + num3 + "<br>")
 
 // Q2
+document.write("<h1>Question 2</h1>")
 
 // Subtraction
 var num4 = num1 - num2
@@ -27,6 +30,30 @@ var num7 = num1 % num2
 document.write("Modulus of " + num1 + " and " + num2 + " is " + num7 + "<br>")
 
 // Q3
+document.write("<h1>Question 3</h1>")
+
+var var1 
+document.write("Value after the variable declaration is " + var1 + "<br>")
+
+var vnum1 = 5
+document.write("Initial value: " + vnum1 + "<br>")
+
+var vnum2 = ++vnum1
+document.write("Value after increment is: " + vnum2 + "<br>")
+
+var vnum3 = vnum1 + 7
+document.write("Value after addition is: " + vnum3 + "<br>")
+
+var vnum4 = --vnum3
+document.write("Value after decrement is: " + vnum4 + "<br>")
+
+var vnum5 = vnum4 % 3
+document.write("The reminder is: " + vnum5 + "<br>")
+
+
+
+
+
 
 
 
