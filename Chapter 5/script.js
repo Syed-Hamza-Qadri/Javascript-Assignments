558dcf083c5e98a934e39c8cc1584d9b85ef6595
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-03 00:24:40 +0500

    Update script.js

--- Chapter 5/script.js
+++ Chapter 5/script.js
@@ -32,7 +32,7 @@ document.write("Modulus of " + num1 + " and " + num2 + " is " + num7 + "<br>")
 // Q3
 document.write("<h2>Question 3</h2>")
 
-var var1 
+var var1
 document.write("Value after the variable declaration is " + var1 + "<br>")
 
 var vnum1 = 5
@@ -60,14 +60,37 @@ document.write("Total cost to buy 5 tickets to a movie is " + cnum1 * 5 + "PKR <
 document.write("<h2>Question 5</h2>")
 
 var tnum1 = 4
-document.write("Table of " + tnum1 + "<br>" + tnum1 +"x1=" + tnum1*1 + "<br>" + tnum1 +"x2=" + tnum1*2 + "<br>" + tnum1 +"x3=" + tnum1*3 + "<br>" + tnum1 +"x4=" + tnum1*4 + "<br>" + tnum1 +"x5=" + tnum1*5 + "<br>" + tnum1 +"x6=" + tnum1*6 + "<br>" + tnum1 +"x7=" + tnum1*7 + "<br>" + tnum1 +"x8=" + tnum1*8 + "<br>" + tnum1 +"x9=" + tnum1*9 + "<br>" + tnum1 +"x10=" + tnum1*10 + "<br>")
+document.write("Table of " + tnum1 + "<br>" + tnum1 + "x1=" + tnum1 * 1 + "<br>" + tnum1 + "x2=" + tnum1 * 2 + "<br>" + tnum1 + "x3=" + tnum1 * 3 + "<br>" + tnum1 + "x4=" + tnum1 * 4 + "<br>" + tnum1 + "x5=" + tnum1 * 5 + "<br>" + tnum1 + "x6=" + tnum1 * 6 + "<br>" + tnum1 + "x7=" + tnum1 * 7 + "<br>" + tnum1 + "x8=" + tnum1 * 8 + "<br>" + tnum1 + "x9=" + tnum1 * 9 + "<br>" + tnum1 + "x10=" + tnum1 * 10 + "<br>")
 
 // Q6
 document.write("<h2>Question 6</h2>")
 
+var temp1 = 25
+var temp2 = (temp1 * 9 / 5) + 32
+document.write(temp1 + "C is " + temp2 + "F" + "<br>")
 
+var temp3 = 70
+var temp4 = (temp3 - 32) * 5 / 9
+document.write(temp3 + "F is " + temp4 + "C" + "<br>")
 
+// Q7
+document.write("<h2>Question 7</h2>")
 
+var inum1 = 650
+var inum2 = 100
+var qnum1 = 3
+var qnum2 = 7
+var snum1 = 100
 
+document.write("<h2>Shopping Cart</h2> <br> Price of item 1 is " + inum1 + "<br> Quantity of item 1 is " + qnum1 + "<br> Price of item 2 is " + inum2 + "<br> Quantity of item 2 is " + qnum2 + "<br> Shipping Charges " + snum1 + "<br>")
 
+// Q8
+document.write("<h2>Question 8</h2>")
 
+var tmarks1 = 980
+var omarks1 = 804
+var percent1 = (omarks1/tmarks1) * 100
+
+document.write("<h2>Mark Sheet</h2> <br> Total Marks : " + tmarks1 + "<br> Marks Obtained : " + omarks1 + "<br> Percentage : " + percent1 + "<br>")
+
+// Q9
