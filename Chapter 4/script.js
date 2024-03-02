2f79f2c5655c55bfafe550da4b0163d6e81ce523
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-02 08:26:10 +0500

    chap 4 updated

--- Chapter 4/script.js
+++ Chapter 4/script.js
@@ -2,15 +2,27 @@
 
 // Q1
 var naam = user = username = "Hamza"
-console.log(naam)
-console.log(user)
-console.log(username)
+console.log(naam + "<br>")
+console.log(user+ "<br>")
+console.log(username+ "<br>")
 
 // Q2
-
+var LVar = "<h1>LEGAL Variables</h1> myVariable <br> _underscoreVariable <br> $dollarVariable <br> camelCaseVariable <br> Number123"
+var IVar = "<h1>ILLEGAL Variables</h1> 123Number (Cannot start with a number)  <br> illegal-variable (Hyphens are not allowed) <br> my variable (Spaces are not allowed) <br> let (Reserved keyword) <br> &specialCharacter (Special characters like '&' are not allowed)"
+console.log(LVar)
+console.log(IVar)
 
 // Q3
+var heading1 = "<h1>Rules for Naming Javascript Variables</h1>"
+var para1 = "Variable names can only contain Letters, Numbers, _ and $. For example $my_1stvariable <br>"
+var para2 = "Variables must begin with a Letters, _ and $. For example $name, _name or name <br>"
+var para3 = "Variable names are case sensitive <br>"
+var para4 = "Variable names should not be JS keywords <br>"
+document.write(heading1)
+document.write(para1)
+document.write(para2)
+document.write(para3)
+document.write(para4)
 
 
-// Q4
 
