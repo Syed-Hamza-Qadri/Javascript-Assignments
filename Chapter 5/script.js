a928056fb79622799427a42aa285f9d27f147b5a
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-02 22:06:07 +0500

    Update script.js

--- Chapter 5/script.js
+++ Chapter 5/script.js
@@ -4,14 +4,14 @@
 var num1 = 3
 var num2 = 5
 
-document.write("<h1>Question 1</h1>")
+document.write("<h2>Question 1</h2>")
 
 // Addition
 var num3 = num1 + num2
 document.write("Sum of " + num1 + " and " + num2 + " is " + num3 + "<br>")
 
 // Q2
-document.write("<h1>Question 2</h1>")
+document.write("<h2>Question 2</h2>")
 
 // Subtraction
 var num4 = num1 - num2
@@ -30,7 +30,7 @@ var num7 = num1 % num2
 document.write("Modulus of " + num1 + " and " + num2 + " is " + num7 + "<br>")
 
 // Q3
-document.write("<h1>Question 3</h1>")
+document.write("<h2>Question 3</h2>")
 
 var var1 
 document.write("Value after the variable declaration is " + var1 + "<br>")
@@ -50,7 +50,20 @@ document.write("Value after decrement is: " + vnum4 + "<br>")
 var vnum5 = vnum4 % 3
 document.write("The reminder is: " + vnum5 + "<br>")
 
+// Q4
+document.write("<h2>Question 4</h2>")
 
+var cnum1 = 600
+document.write("Total cost to buy 5 tickets to a movie is " + cnum1 * 5 + "PKR <br>")
+
+// Q5 
+document.write("<h2>Question 5</h2>")
+
+var tnum1 = 4
+document.write("Table of 4 <br> " + tnum1 +"x1=" + tnum1*1 + "<br>" + tnum1 +"x2=" + tnum1*2 + "<br>" + tnum1 +"x3=" + tnum1*3 + "<br>" + tnum1 +"x4=" + tnum1*4 + "<br>" + tnum1 +"x5=" + tnum1*5 + "<br>" + tnum1 +"x6=" + tnum1*6 + "<br>" + tnum1 +"x7=" + tnum1*7 + "<br>" + tnum1 +"x8=" + tnum1*8 + "<br>" + tnum1 +"x9=" + tnum1*9 + "<br>" + tnum1 +"x10=" + tnum1*10 + "<br>")
+
+// Q6
+document.write("<h2>Question 6</h2>")
 
 
 
