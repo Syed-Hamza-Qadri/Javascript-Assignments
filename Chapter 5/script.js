09ea4c77e5c40d1009a19244ce61d272a6cca577
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-02 21:30:45 +0500

    Update script.js

--- Chapter 5/script.js
+++ Chapter 5/script.js
@@ -1,10 +1,30 @@
 // Chapter 5
 
 // Q1
+var num1 = 3
+var num2 = 5
 
+// Addition
+var num3 = num1 + num2
+document.write("Sum of " + num1 + " and " + num2 + " is " + num3 + "<br>")
 
 // Q2
 
+// Subtraction
+var num4 = num1 - num2
+document.write("Subtraction of " + num1 + " and " + num2 + " is " + num4 + "<br>")
+
+// Multiplication
+var num5 = num1 * num2
+document.write("Product of " + num1 + " and " + num2 + " is " + num5 + "<br>")
+
+// Division
+var num6 = num1 / num2
+document.write("Divison of " + num1 + " and " + num2 + " is " + num6 + "<br>")
+
+// Modulus
+var num7 = num1 % num2
+document.write("Modulus of " + num1 + " and " + num2 + " is " + num7 + "<br>")
 
 // Q3
 
