1bc4fb0bf205afe34178fb44c5dbc89c7d22be59
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-07 08:15:13 +0500

    Update script.js

--- Chapter 12 & 13/script.js
+++ Chapter 12 & 13/script.js
@@ -1,8 +1,7 @@
 // Chapter 12 & 13
 
-// Q1
-document.write("<h2>Question 1</h2>")
-
+// // Q1
+// document.write("<h2>Question 1</h2>")
 
 // Q2
 document.write("<h2>Question 2</h2>")
@@ -28,7 +27,7 @@ if (isNaN(int1) || isNaN(int2)) {
 // Q3
 document.write("<h2>Question 3</h2>")
 
-var userInput3 = +prompt("Enter a number:")
+var userInput2 = +prompt("Enter a number:")
 
 if (isNaN(userInput2)) {
     document.write(`${userInput2} is a invalid number.`)
@@ -47,5 +46,37 @@ if (isNaN(userInput2)) {
 // // Q4
 // document.write("<h2>Question 4</h2>")
 
-// // Q5 
-// document.write("<h2>Question 5</h2>")
+// Q5 
+document.write("<h2>Question 5</h2>")
+
+var pass1 = "Password"
+var uinput = prompt("Enter your password:")
+
+if (!uinput) {
+    document.write("Please enter your password.")
+    console.log("Please enter your password.")
+} else if (uinput === pass1) {
+    document.write("Correct! The password you entered matches the original password.")
+    console.log("Correct! The password you entered matches the original password.")
+} else {
+    document.write("Incorrect password.")
+    console.log("Incorrect password.")
+}
+
+// Q6
+document.write("<h2>Question 6</h2>")
+
+var greeting;
+var hour = 13;
+if (hour < 18) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+console.log(greeting)
+document.write(greeting)
+
+// // Q7
+// document.write("<h2>Question 7</h2>")
+
